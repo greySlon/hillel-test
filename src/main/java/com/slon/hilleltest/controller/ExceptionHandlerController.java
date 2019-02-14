8628a79e29ff9c96d64b8743329a5a657ce10549
GreySlon <garanenkosergii@gmail.com>
2019-02-14 02:10:40 +0200

    add request object validation

--- src/main/java/com/slon/hilleltest/controller/ExceptionHandlerController.java
+++ src/main/java/com/slon/hilleltest/controller/ExceptionHandlerController.java
@@ -3,9 +3,12 @@ package com.slon.hilleltest.controller;
 import com.slon.hilleltest.domain.AppResponse;
 import com.slon.hilleltest.exception.AppException;
 
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 @RestController
@@ -16,5 +19,13 @@ public class ExceptionHandlerController {
     String message = ex.getMessage();
     int status = ex.getStatus();
     return new AppResponse(status, message);
+  }
+
+  @ExceptionHandler(value = {MethodArgumentNotValidException.class})
+  public AppResponse exceptionHandler(MethodArgumentNotValidException ex) {
+    String message = ex.getBindingResult().getAllErrors().stream()
+        .map(er -> er.getDefaultMessage())
+        .collect(Collectors.joining("; "));
+    return new AppResponse(400, message);
   }
 }
--- src/main/java/com/slon/hilleltest/domain/UserDto.java
+++ src/main/java/com/slon/hilleltest/domain/UserDto.java
@@ -4,17 +4,24 @@ import lombok.Data;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import javax.validation.constraints.NotNull;
+
 @Data
 public class UserDto {
 
+  @NotNull(message = "Firstname shouldn't be null")
   @JsonProperty(value = "firstname")
   private String firstName;
+  @NotNull(message = "Lastname shouldn't be null")
   @JsonProperty(value = "lastname")
   private String lastName;
+  @NotNull(message = "Country should be specified")
   @JsonProperty(value = "country")
   private String country;
+  @NotNull(message = "City should be specified")
   @JsonProperty(value = "city")
   private String city;
+  @NotNull(message = "Street should be specified")
   @JsonProperty(value = "street")
   private String street;
 }
